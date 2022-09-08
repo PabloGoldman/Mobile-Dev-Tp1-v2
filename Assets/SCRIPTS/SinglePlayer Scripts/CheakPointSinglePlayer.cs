29288528308067153778883fa48b57ecb273c601
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-09-08 20:11:46 -0300

    Updated Respawn singleplayer script

---
+++ Assets/SCRIPTS/SinglePlayer Scripts/CheakPointSinglePlayer.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheakPointSinglePlayer : MonoBehaviour
+{
+	public string PlayerTag = "Player";
+	bool HabilitadoResp = true;
+	public float TiempPermanencia = 0.7f;//tiempo que no deja respaunear a un pj desp que el otro lo hizo.
+	float Tempo = 0;
+
+	// Use this for initialization
+	void Start()
+	{
+		GetComponent<Renderer>().enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!HabilitadoResp)
+		{
+			Tempo += T.GetDT();
+			if (Tempo >= TiempPermanencia)
+			{
+				Tempo = 0;
+				HabilitadoResp = true;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == PlayerTag)
+		{
+			other.GetComponent<RespawnSinglePlayer>().AgregarCP(this);
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == PlayerTag)
+		{
+			HabilitadoResp = true;
+		}
+	}
+
+	//---------------------------------------------------//
+
+	public bool Habilitado()
+	{
+		if (HabilitadoResp)
+		{
+			HabilitadoResp = false;
+			Tempo = 0;
+			return true;
+		}
+		else
+		{
+			return HabilitadoResp;
+		}
+	}
+}
