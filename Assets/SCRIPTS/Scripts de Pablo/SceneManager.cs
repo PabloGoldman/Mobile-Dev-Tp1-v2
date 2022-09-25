56f6e320b8c1222228960ecc7e360a09451789a2
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-09-25 18:51:25 -0300

    Fixed not reseting timeScale

--- Assets/SCRIPTS/Scripts de Pablo/SceneManager.cs
+++ Assets/SCRIPTS/Scripts de Pablo/SceneManager.cs
@@ -6,11 +6,13 @@ namespace Managers
     {
         public void ChangeScene(int index)
         {
+            Time.timeScale = 1;
             UnityEngine.SceneManagement.SceneManager.LoadScene(index);
         }
 
         public void ResetLevel()
         {
+            Time.timeScale = 1;
             UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
         }
 
